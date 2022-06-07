3a437bd3a0c3f07d488e9811a6ba1ea965c1eb0e
Anuj Gusain <anujgusain108@gmail.com>
2022-06-07 20:53:51 +0530

    nextPermutation

---
+++ nextPermutation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    //  Write your code here.
+    vector<int> ans = permutation;
+    int k ,l;
+
+    for(k = n-2 ; k>=0 ;k--) {
+        if(ans[k] < ans[k+1]) {
+            break;
+        }
+    }
+    if(k < 0 ) {
+        reverse(ans.begin(),ans.end());
+    }
+    else{
+        for(l = n-1; l>=0;l--) {
+            if(ans[l] > ans[k]) {
+                break;
+            }
+        }
+        swap(ans[k],ans[l]);
+        reverse(ans.begin()+k+1,ans.end());
+    }
+    return ans;
+}
